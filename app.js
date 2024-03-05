9cc037f73d08046062090c5d89103f85df39fb3c
sandeshmurdia <sandeshmurdia123@gmail.com>
2024-03-05 13:38:03 +0530

    changes

--- app.js
+++ app.js
@@ -30,6 +30,26 @@ app.post('/doc', (req, res) => {
   res.json(responseObject);
 });
 
+app.get('/doc', (req, res) => {
+    // Set custom response headers
+    res.set({
+      'X-Content-Type-Options': 'nosniff',
+      'Access-Control-Expose-Headers' : 'Server',
+      'Content-Type' : 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    });
+  
+    // Create a JSON object
+    const responseObject = {
+      message: 'This is a documentation endpoint',
+      author: 'Your Name',
+      date: new Date()
+    };
+  
+    // Send the JSON response
+    res.json(responseObject);
+  });
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
